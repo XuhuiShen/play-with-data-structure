3ea63e71ea4309688e36358ff0b4a418ca15c465
shen <shenxuhui1992@163.com>
2017-05-12 20:57:40 +0800

    add get_element function

--- linear-list/sequential-storage/main.c
+++ linear-list/sequential-storage/main.c
@@ -6,3 +6,11 @@ typedef struct {
 	element_type data[MAXSIZE];
 	int length;
 } sequential_list;
+
+int get_element(sequential_list l, int i, element_type *e)
+{
+	if (l.length == 0 || i > l.length)
+		return -1;
+	*e = l.data[i - 1];
+	return 0;
+}
