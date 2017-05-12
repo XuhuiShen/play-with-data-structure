a75f5dbda2658190174ec4f4fd68e1868fe9aeec
shen <shenxuhui1992@163.com>
2017-05-12 21:55:27 +0800

    add test_get_element function

--- linear-list/sequential-storage/test/test.c
+++ linear-list/sequential-storage/test/test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unit_test.h>
-#include <../main.c>
+#include <../list.c>
 
 sequential_list l;
 
@@ -10,13 +10,17 @@ int unit_test_init()
 	int i;
 	for (i = 0; i < l.length; i++)
 		l.data[i] = i + 1;
+	return 0;
 }
 
 unit_test(test_get_element)
 {
-	int a = 1;
-	printf("test a = %d\n", a);
-	test_assert(a == 1);
+	int i;
+	element_type e = 0;
+	for (i = 0; i < l.length; i++) {
+		get_element(l, i, &e);
+		test_assert(e == i + 1);
+	}
 }
 
 test_file_end
