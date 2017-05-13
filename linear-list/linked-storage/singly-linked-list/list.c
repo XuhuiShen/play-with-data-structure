d3a7fa451cbacbc844b3a650041f9a929fb89c72
shen <shenxuhui1992@163.com>
2017-05-13 12:24:47 +0800

    improve code readability

--- linear-list/linked-storage/singly-linked-list/list.c
+++ linear-list/linked-storage/singly-linked-list/list.c
@@ -7,11 +7,11 @@ typedef struct node {
 	struct node *next;
 } node;
 
-typedef struct node *link_list_node;
+typedef struct node *link_list;
 
-int get_element(link_list_node l, int i, element_type *e)
+int get_element(link_list l, int i, element_type *e)
 {
-	link_list_node p;
+	node *p;
 	p = l->next;
 	int j;
 	for (j = 1; p && j < i; j++)
@@ -22,16 +22,16 @@ int get_element(link_list_node l, int i, element_type *e)
 	return 0;
 }
 
-int insert_element(link_list_node *l, int i, element_type e)
+int insert_element(link_list *l, int i, element_type e)
 {
-	link_list_node p, s;
+	node *p, *s;
 	p = *l;
 	int j;
 	for (j = 1; p && j < i; j++)
 		p = p->next;
 	if (!p || j > i)
 		return -1;
-	s = (link_list_node)malloc(sizeof(node));
+	s = (node *)malloc(sizeof(node));
 	s->data = e;
 	s->next = p->next;
 	p->next = s;
