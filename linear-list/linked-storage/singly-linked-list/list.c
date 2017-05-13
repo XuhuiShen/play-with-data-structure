385ee5eb51daff2070f3062cdee06579f4fe84f1
shen <shenxuhui1992@163.com>
2017-05-13 14:04:34 +0800

    add create_list function

--- linear-list/linked-storage/singly-linked-list/list.c
+++ linear-list/linked-storage/singly-linked-list/list.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef int element_type;
 
@@ -55,4 +57,36 @@ int delete_element(link_list *l, int i, element_type *e)
 	free(q);
 
 	return 0;
+}
+
+/* Establishing a list which has lead node and random value of elements. */
+void create_list_head_insert(link_list *l, int n)
+{
+	node *p;
+	int i;
+	srand(time(0));
+	*l = (link_list)malloc(sizeof(node));
+	(*l)->next = NULL;
+	for (i = 0; i < n; i++) {
+		p = (link_list)malloc(sizeof(node));
+		p->data = rand() % 100 + 1;
+		p->next = (*l)->next;
+		(*l)->next = p;
+	}
+}
+
+void create_list_tail_insert(link_list *l, int n)
+{
+	node *p;
+	int i;
+	srand(time(0));
+	*l = (link_list)malloc(sizeof(node));
+	node *r = *l;
+	for (i = 0; i < n; i++) {
+		p = (link_list)malloc(sizeof(node));
+		p->data = rand() % 100 + 1;
+		r->next = p;
+		r = p;
+	}
+	r->next = NULL;
 }
