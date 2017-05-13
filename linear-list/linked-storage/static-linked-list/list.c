97cb46f4b2e219ad1d03c722043fd6750ce6b5bd
shen <shenxuhui1992@163.com>
2017-05-13 20:16:37 +0800

    add static-linked-list

---
+++ linear-list/linked-storage/static-linked-list/list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define MAXSIZE 1000
+
+typedef int element_type;
+
+typedef struct {
+	element_type data;
+	int cur;
+} component, static_link_list[MAXSIZE];
+
+int init(static_link_list space)
+{
+	int i;
+	for (i = 0; i < MAXSIZE; i++)
+		space[i].cur = i + 1;
+	space[MAXSIZE - 1].cur = 0;
+
+	return 0;
+}
