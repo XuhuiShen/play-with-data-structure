3d4813c44276a755aec346915c0d84ad21e9b592
shen <shenxuhui1992@163.com>
2017-05-15 16:13:48 +0800

    update test for general stack

--- stack/sequential-storage/general-stack/test/test.c
+++ stack/sequential-storage/general-stack/test/test.c
@@ -8,19 +8,37 @@ void print_stack()
 {
 	int i;
 	for (i = 0; i < MAXSIZE; i++)
-		printf("sequential_stack: %d\n", s->data[i]);
+		printf("sequential_stack: %d\n", s.data[i]);
 }
 
 int unit_test_init()
 {
 	int i;
 	for (i = 0; i < MAXSIZE; i++)
-		s->data[i] = i;
+		s.data[i] = i;
+	s.top = MAXSIZE - 1;
+	return 0;
 }
 
 unit_test(test_pop)
 {
 	unit_test_init();
+	element_type e = 0;
+	int i;
+	for (i = 0; i < MAXSIZE; i++) {
+		pop(&s, &e);
+		test_assert(e == MAXSIZE -1 - i);
+	}
 }
+
+unit_test(test_push)
+{
+	int i;
+	for (i = 0; i < MAXSIZE; i++)
+		push(&s, i);
+	for (i = 0; i < MAXSIZE; i++)
+		test_assert(s.data[i] == i);
+}
+
 
 test_file_end
