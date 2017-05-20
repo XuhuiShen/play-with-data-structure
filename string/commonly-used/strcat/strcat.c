411ce1a0a7fe948cd990fff2d2ce4fde0cbf83ef
shen <shenxuhui1992@163.com>
2017-05-20 22:10:37 +0800

    string function don't need to care about overflow.

--- string/commonly-used/strcat/strcat.c
+++ string/commonly-used/strcat/strcat.c
@@ -1,22 +1,22 @@
 #include <string.h>
 #include <stdio.h>
 
-char *_strcat(char *s1, const char *s2)
+char *strcat(char *s1, const char *s2)
 {
-	char *s = malloc(strlen(s1) + strlen(s2));
-	strcpy(s, s1);
-	char *_s = s;
-	while (*_s != '\0')
-		_s++;
-	strcpy(_s, s2);
-	return s;
+	char *s = s1;
+	while (*s != '\0')
+		s++;
+	strcpy(s, s2);
+	return s1;
 }
 
 int main()
 {
 	char *s1 = "hello ";
 	char *s2 = "world";
-	char *r = _strcat(s1, s2);
+	char *s = malloc(sizeof(strlen(s1) + strlen(s2) + 1));
+	strcpy(s, s1);
+	char *r = strcat(s, s2);
 	printf("%s\n", r);
 
 	return 0;
