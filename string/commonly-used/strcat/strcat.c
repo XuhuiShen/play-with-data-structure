33d946cc80a10c89c1c355a2876030d32ce24353
shen <shenxuhui1992@163.com>
2017-05-20 21:12:33 +0800

    add test for strcat function.

--- string/commonly-used/strcat/strcat.c
+++ string/commonly-used/strcat/strcat.c
@@ -1,12 +1,23 @@
 #include <string.h>
+#include <stdio.h>
 
-char *strcat(char *restrict s1, const char *restrict s2)
+char *_strcat(char *s1, const char *s2)
 {
-	char *s = s1;
-	/* Move s so that it points to the end of s1.  */
-	while (*s != '\0')
-		s++;
-	/* Copy the contents of s2 into the space at the end of s1.  */
-	strcpy(s, s2);
-	return s1;
+	char *s = malloc(strlen(s1) + strlen(s2));
+	strcpy(s, s1);
+	char *_s = s;
+	while (*_s != '\0')
+		_s++;
+	strcpy(_s, s2);
+	return s;
+}
+
+int main()
+{
+	char *s1 = "hello ";
+	char *s2 = "world";
+	char *r = _strcat(s1, s2);
+	printf("%s\n", r);
+
+	return 0;
 }
