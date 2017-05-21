95aef58b0ee42e6bcc99bf61202b2e72548c7525
shen <shenxuhui1992@163.com>
2017-05-21 23:13:32 +0800

    add strchr.

---
+++ string/commonly-used/strchr/strchr.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+
+char *_strchr(const char *s, int c)
+{
+	char ch = c;
+	while (*s != '\0' && *s != ch)
+		s++;
+
+	return (*s == ch) ? (char *) s : NULL;
+}
+
+int main()
+{
+	char *s = "apple";
+	char c = 'l';
+	char *p = _strchr(s, c);
+	printf("%c\n", *p);
+
+	return 0;
+}
